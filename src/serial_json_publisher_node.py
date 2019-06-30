b18fb5bd11bfeb58457fc9c7ad1767e9f35130e3
fatahisani <fattahi.m91@gmail.com>
2019-06-30 02:40:28 +0100

    change file to recieve and publish json style

--- src/serial_json_publisher_node.py
+++ src/serial_json_publisher_node.py
@@ -71,8 +71,84 @@ def talker():
 		print("raw data:{}".format(line))
 		if len(line)>13:
 			dataj = json.loads(line)
-			print(dataj)
-			print(dataj['S0'])
+			# print(dataj)
+			# print(dataj['S0'])
+
+			Sensor_pose0 = Pose()
+			Sensor_pose0.position.x = float(dataj['S0'][0]) / 10
+			Sensor_pose0.position.y = float(dataj['S0'][1]) / 10
+			Sensor_pose0.position.z = float(dataj['S0'][2]) / 10
+
+			Sensor_pose1 = Pose()
+			Sensor_pose1.position.x = float(dataj['S1'][0]) / 10
+			Sensor_pose1.position.y = float(dataj['S1'][1]) / 10
+			Sensor_pose1.position.z = float(dataj['S1'][2]) / 10
+
+			Sensor_pose2 = Pose()
+			Sensor_pose2.position.x = float(dataj['S2'][0]) / 10
+			Sensor_pose2.position.y = float(dataj['S2'][1]) / 10
+			Sensor_pose2.position.z = float(dataj['S2'][2]) / 10
+
+			Sensor_pose3 = Pose()
+			Sensor_pose3.position.x = float(dataj['S3'][0]) / 10
+			Sensor_pose3.position.y = float(dataj['S3'][1]) / 10
+			Sensor_pose3.position.z = float(dataj['S3'][2]) / 10
+
+			Sensor_pose4 = Pose()
+			Sensor_pose4.position.x = float(dataj['S4'][0]) / 10
+			Sensor_pose4.position.y = float(dataj['S4'][1]) / 10
+			Sensor_pose4.position.z = float(dataj['S4'][2]) / 10
+
+			Sensor_pose5 = Pose()
+			Sensor_pose5.position.x = float(dataj['S5'][0]) / 10
+			Sensor_pose5.position.y = float(dataj['S5'][1]) / 10
+			Sensor_pose5.position.z = float(dataj['S5'][2]) / 10
+
+			Sensor_pose6 = Pose()
+			Sensor_pose6.position.x = float(dataj['S6'][0]) / 10
+			Sensor_pose6.position.y = float(dataj['S6'][1]) / 10
+			Sensor_pose6.position.z = float(dataj['S6'][2]) / 10
+
+			Sensor_pose7 = Pose()
+			Sensor_pose7.position.x = float(dataj['S7'][0]) / 10
+			Sensor_pose7.position.y = float(dataj['S7'][1]) / 10
+			Sensor_pose7.position.z = float(dataj['S7'][2]) / 10
+
+			Sensor_pose8 = Pose()
+			Sensor_pose8.position.x = float(dataj['S8'][0]) / 10
+			Sensor_pose8.position.y = float(dataj['S8'][1]) / 10
+			Sensor_pose8.position.z = float(dataj['S8'][2]) / 10
+
+			Sensor_pose9 = Pose()
+			Sensor_pose9.position.x = float(dataj['S9'][0]) / 10
+			Sensor_pose9.position.y = float(dataj['S9'][1]) / 10
+			Sensor_pose9.position.z = float(dataj['S9'][2]) / 10
+
+			Sensor_pose10 = Pose()
+			Sensor_pose10.position.x = float(dataj['S10'][0]) / 10
+			Sensor_pose10.position.y = float(dataj['S10'][1]) / 10
+			Sensor_pose10.position.z = float(dataj['S10'][2]) / 10
+
+			Sensor_pose11 = Pose()
+			Sensor_pose11.position.x = float(dataj['S11'][0]) / 10
+			Sensor_pose11.position.y = float(dataj['S11'][1]) / 10
+			Sensor_pose11.position.z = float(dataj['S11'][2]) / 10
+
+			pub_S0.publish(Sensor_pose0.position)
+			pub_S1.publish(Sensor_pose1.position)
+			pub_S2.publish(Sensor_pose2.position)
+			pub_S3.publish(Sensor_pose3.position)
+			pub_S4.publish(Sensor_pose4.position)
+			pub_S5.publish(Sensor_pose5.position)
+			pub_S6.publish(Sensor_pose6.position)
+			pub_S7.publish(Sensor_pose7.position)
+			pub_S8.publish(Sensor_pose8.position)
+			pub_S9.publish(Sensor_pose9.position)
+			pub_S10.publish(Sensor_pose10.position)
+			pub_S11.publish(Sensor_pose11.position)
+
+			
+
 
 		# print type(dataj)
 		# rospy.loginfo(line)
@@ -93,14 +169,11 @@ def talker():
 		# 	S0BY = lowbyte_highbyte_to_float(line[9], line[10])
 		# 	S0BX = lowbyte_highbyte_to_float(line[11], line[12])
 		# 	# print("S0AX: {}, Y: {} ,Z: {}; S0BX: {}, Y:{}, Z:{}".format(S0AX,S0AY,S0AZ,S0BX,S0BY,S0BZ))
-		# 	Sensor_poseA = Pose()
-		# 	Sensor_poseA.position.x = float(S0AX) / 10
-		# 	Sensor_poseA.position.y = float(S0AY) / 10
-		# 	Sensor_poseA.position.z = float(S0AZ) / 10
-		#
-		# 	Sensor_poseB = Pose()
-		# 	Sensor_poseB.position.x = float(S0BX) / 10
-		# 	Sensor_poseB.position.y = float(S0BY) / 10
+
+
+			# Sensor_poseB = Pose()
+			# Sensor_poseB.position.x = float(S0BX) / 10
+			# Sensor_poseB.position.y = float(S0BY) / 10
 		# 	Sensor_poseB.position.z = float(S0BZ) / 10
 		#
 		# 	# sensorA_point=Point()
