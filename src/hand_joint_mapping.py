d5ee03bdcc43cb0dbec75c359cecdfb09bcaec8d
fatahisani <fattahi.m91@gmail.com>
2019-06-30 01:50:28 +0100

    some changes!

---
+++ src/hand_joint_mapping.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import MarkerArray, Marker
+from odas_msgs.msg import Odas, OdasList
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseArray
+from std_msgs.msg import Float64
+
+from geometry_msgs.msg import TwistStamped
+import numpy
+
+import math
+
+
+pub_rh_ffj0=0
+pub_rh_ffj3=0
+pub_rh_ffj4=0
+pub_rh_mfj0=0
+pub_rh_mfj3=0
+pub_rh_mfj4=0
+pub_rh_thj1=0
+pub_rh_thj2=0
+pub_rh_thj3=0
+pub_rh_thj4=0
+pub_rh_wrj1=0
+pub_rh_wrj2=0
+
+calibration_counter=0
+
+
+Index_DIP_fl_buff=[]
+Index_PIP_fl_buff=[]
+Index_MIP_fl_buff=[]
+Index_MIP_ab_buff=[]
+Middle_DIP_fl_buff=[]
+Middle_PIP_fl_buff=[]
+Middle_MIP_fl_buff=[]
+Middle_MIP_ab_buff=[]
+Thumb_DIP_fl_buff=[]
+Thumb_MIP_fl_buff=[]
+Thumb_CMC_fl_buff=[]
+Thumb_CMC_ab_buff=[]
+Wrist_JOINT_fl_buff=[]
+Wrist_JOINT_ab_buff=[]
+
+Index_DIP_fl_offset=0
+Index_PIP_fl_offset=0
+Index_MIP_fl_offset=0
+Index_MIP_ab_offset=0
+Middle_DIP_fl_offset=0
+Middle_PIP_fl_offset=0
+Middle_MIP_fl_offset=0
+Middle_MIP_ab_offset=0
+Thumb_DIP_fl_offset=0
+Thumb_MIP_fl_offset=0
+Thumb_CMC_fl_offset=0
+Thumb_CMC_ab_offset=0
+Wrist_JOINT_fl_offset=0
+Wrist_JOINT_ab_offset=0
+
+
+def calc_joints(data1,data2):
+    output=Point()
+    output.x=data2.position.x-data1.position.x
+    output.y=data2.position.y-data1.position.y
+    output.z=data2.position.z-data1.position.z
+    return output
+
+
+
+def imu_serial_callback(data):
+    global pub_rh_ffj0
+    global pub_rh_ffj3
+    global pub_rh_ffj4
+    global pub_rh_mfj0
+    global pub_rh_mfj3
+    global pub_rh_mfj4
+    global pub_rh_thj1
+    global pub_rh_thj2
+    global pub_rh_thj3
+    global pub_rh_thj4
+    global pub_rh_wrj1
+    global pub_rh_wrj2
+
+    global calibration_counter
+
+    global Index_DIP_fl_buff
+    global Index_PIP_fl_buff
+    global Index_MIP_fl_buff
+    global Index_MIP_ab_buff
+    global Middle_DIP_fl_buff
+    global Middle_PIP_fl_buff
+    global Middle_MIP_fl_buff
+    global Middle_MIP_ab_buff
+    global Thumb_DIP_fl_buff
+    global Thumb_MIP_fl_buff
+    global Thumb_CMC_fl_buff
+    global Thumb_CMC_ab_buff
+    global Wrist_JOINT_fl_buff
+    global Wrist_JOINT_ab_buff
+
+    global Index_DIP_fl_offset
+    global Index_PIP_fl_offset
+    global Index_MIP_fl_offset
+    global Index_MIP_ab_offset
+    global Middle_DIP_fl_offset
+    global Middle_PIP_fl_offset
+    global Middle_MIP_fl_offset
+    global Middle_MIP_ab_offset
+    global Thumb_DIP_fl_offset
+    global Thumb_MIP_fl_offset
+    global Thumb_CMC_fl_offset
+    global Thumb_CMC_ab_offset
+    global Wrist_JOINT_fl_offset
+    global Wrist_JOINT_ab_offset
+
+    # for sensor_id, sensor_values in enumerate(data.poses):
+    #     print("sensor{}:{}".format(sensor_id, sensor_values))
+    rospy.loginfo("test")
+    # % Index                                           #full sensor model
+    Index_DIP = calc_joints(data.poses[3],data.poses[2])#3-2
+    Index_PIP = calc_joints(data.poses[3],data.poses[2])#2-8
+    Index_MIP = calc_joints(data.poses[6],data.poses[3])#8-7
+
+    Index_DIP_fl =Index_DIP.x
+    Index_PIP_fl =Index_PIP.x
+    Index_MIP_fl =Index_MIP.x
+    Index_MIP_ab =Index_MIP.z
+
+    # % Middle
+    Middle_DIP = calc_joints(data.poses[5],data.poses[4])#5-4
+    Middle_PIP = calc_joints(data.poses[5],data.poses[4])#4-10
+    Middle_MIP = calc_joints(data.poses[6],data.poses[5])#10-7
+
+    Middle_DIP_fl=Middle_DIP.x
+    Middle_PIP_fl=Middle_PIP.x
+    Middle_MIP_fl=Middle_MIP.x
+    Middle_MIP_ab=Middle_MIP.z
+
+    # % Thumb
+    Thumb_DIP = calc_joints(data.poses[1],data.poses[0])#1-0
+    Thumb_MIP = calc_joints(data.poses[1],data.poses[0])#0-7
+    Thumb_CMC = calc_joints(data.poses[6],data.poses[1])#7-6
+
+    Thumb_DIP_fl = Thumb_DIP.x
+    Thumb_MIP_fl = Thumb_MIP.x
+    Thumb_CMC_fl = Thumb_CMC.x
+    Thumb_CMC_ab = Thumb_CMC.z
+
+    # % wrist
+    Wrist_JOINT = calc_joints(data.poses[7],data.poses[6])#9-10
+    Wrist_JOINT_fl=Wrist_JOINT.x
+    Wrist_JOINT_ab=Wrist_JOINT.z
+
+
+    if calibration_counter<100:
+        Index_DIP_fl_buff.append(Index_DIP_fl)
+        Index_PIP_fl_buff.append(Index_PIP_fl)
+        Index_MIP_fl_buff.append(Index_MIP_fl)
+        Index_MIP_fl_buff.append(Index_MIP_ab)
+
+        Middle_DIP_fl_buff.append(Middle_DIP_fl)
+        Middle_PIP_fl_buff.append(Middle_PIP_fl)
+        Middle_MIP_fl_buff.append(Middle_MIP_fl)
+        Middle_MIP_fl_buff.append(Middle_MIP_ab)
+
+        Thumb_DIP_fl_buff.append(Thumb_DIP_fl)
+        Thumb_MIP_fl_buff.append(Thumb_MIP_fl)
+        Thumb_CMC_fl_buff.append(Thumb_CMC_fl)
+        Thumb_CMC_fl_buff.append(Thumb_CMC_ab)
+
+        Wrist_JOINT_fl_buff.append(Wrist_JOINT_fl)
+        Wrist_JOINT_fl_buff.append(Wrist_JOINT_ab)
+
+        Index_DIP_fl_offset=numpy.mean(Index_DIP_fl_buff)
+        Index_PIP_fl_offset=numpy.mean(Index_PIP_fl_buff)
+        Index_MIP_fl_offset=numpy.mean(Index_MIP_fl_buff)
+        Index_MIP_ab_offset=numpy.mean(Index_MIP_ab_buff)
+        Middle_DIP_fl_offset=numpy.mean(Middle_DIP_fl_buff)
+        Middle_PIP_fl_offset=numpy.mean(Middle_PIP_fl_buff)
+        Middle_MIP_fl_offset=numpy.mean(Middle_MIP_fl_buff)
+        Middle_MIP_ab_offset=numpy.mean(Middle_MIP_ab_buff)
+        Thumb_DIP_fl_offset=numpy.mean(Thumb_DIP_fl_buff)
+        Thumb_MIP_fl_offset=numpy.mean(Thumb_MIP_fl_buff)
+        Thumb_CMC_fl_offset=numpy.mean(Thumb_CMC_fl_buff)
+        Thumb_CMC_ab_offset=numpy.mean(Thumb_CMC_ab_buff)
+        Wrist_JOINT_fl_offset=numpy.mean(Wrist_JOINT_fl_buff)
+        Wrist_JOINT_ab_offset=numpy.mean(Wrist_JOINT_ab_buff)
+
+        calibration_counter=calibration_counter+1
+
+
+    else :
+        pub_rh_ffj0.publish(math.radians(Index_PIP_fl-Index_PIP_fl_offset))
+        pub_rh_ffj3.publish(math.radians(Index_MIP_fl-Index_MIP_fl_offset))
+        # pub_rh_ffj4.publish(math.radians(Index_MIP_ab-Index_MIP_ab_offset))
+        # print("Index_PIP_fl:{},Index_PIP_fl_offset:{}".format(Index_PIP_fl,Index_PIP_fl_offset.item()))
+        # print("Index_PIP_fl:{},Index_MIP_fl:{},Index_MIP_ab:{}".format(Index_PIP_fl-Index_PIP_fl_offset,
+        #                                                                Index_MIP_fl-Index_MIP_fl_offset,
+        #                                                                Index_MIP_ab-Index_MIP_ab_offset))
+
+        # pub_rh_mfj0.publish(math.radians(Middle_PIP_fl - Middle_PIP_fl_offset))
+        # pub_rh_mfj3.publish(math.radians(Middle_MIP_fl - Middle_MIP_fl_offset))
+        # pub_rh_mfj4.publish(math.radians(Middle_MIP_ab - Middle_MIP_ab_offset))
+        # print("Middle_PIP_fl:{},Middle_MIP_fl:{},Middle_MIP_ab:{}".format(Middle_PIP_fl - Middle_PIP_fl_offset,
+        #                                                                Middle_MIP_fl - Middle_MIP_fl_offset,
+        #                                                                Middle_MIP_ab - Middle_MIP_ab_offset))
+
+        # pub_rh_thj1.publish(math.radians(Thumb_DIP_fl - Thumb_DIP_fl_offset))
+        # pub_rh_thj2.publish(math.radians(Thumb_MIP_fl - Thumb_MIP_fl_offset))
+        # pub_rh_thj4.publish(math.radians(Thumb_CMC_ab - Thumb_CMC_ab_offset))
+        # print("Thumb_PIP_fl:{},Thumb_MIP_fl:{},Thumb_MIP_ab:{}".format(Thumb_DIP_fl - Thumb_DIP_fl_offset,
+        #                                                                Thumb_MIP_fl - Thumb_MIP_fl_offset,
+        #                                                                Thumb_CMC_ab - Thumb_CMC_ab_offset))
+
+        # pub_rh_wrj1.publish(math.radians(Wrist_JOINT_fl-Wrist_JOINT_fl_offset))
+        # pub_rh_wrj2.publish(math.radians(Wrist_JOINT_ab-Wrist_JOINT_ab_offset))
+        # print("Wrist_JOINT_fl_buff:{},Wrist_JOINT_ab_buff:{}".format(Wrist_JOINT_fl-Wrist_JOINT_fl_offset,
+        #                                         Wrist_JOINT_ab-Wrist_JOINT_ab_offset))
+
+def main():
+
+    rospy.init_node('hand_joint_mapping', anonymous=True)
+
+    # rospy.Subscriber("/odas_sst_raw", Odas, odas_snobber_Callback, queue_size=10)
+    rospy.Subscriber("/imu_pub_array", PoseArray, imu_serial_callback, queue_size=10)
+    # global listener
+    global pub_rh_ffj0
+    global pub_rh_ffj3
+    global pub_rh_ffj4
+    global pub_rh_mfj0
+    global pub_rh_mfj3
+    global pub_rh_mfj4
+    global pub_rh_thj1
+    global pub_rh_thj2
+    global pub_rh_thj3
+    global pub_rh_thj4
+    global pub_rh_wrj1
+    global pub_rh_wrj2
+
+
+    # listener = tf.TransformListener()
+    pub_rh_ffj0 = rospy.Publisher("/sh_rh_ffj0_position_controller/command", Float64, queue_size=10) #index pip
+    pub_rh_ffj3 = rospy.Publisher("/sh_rh_ffj3_position_controller/command", Float64, queue_size=10)#index mix flex
+    pub_rh_ffj4 = rospy.Publisher("/sh_rh_ffj4_position_controller/command", Float64, queue_size=10)#index mix abd
+
+    pub_rh_mfj0 = rospy.Publisher("/sh_rh_mfj0_position_controller/command", Float64, queue_size=10) #middle pip
+    pub_rh_mfj3 = rospy.Publisher("/sh_rh_mfj3_position_controller/command", Float64, queue_size=10)#middle mix flex
+    pub_rh_mfj4 = rospy.Publisher("/sh_rh_mfj4_position_controller/command", Float64, queue_size=10)#middle mix abd
+
+    pub_rh_thj1 = rospy.Publisher("/sh_rh_thj1_position_controller/command", Float64, queue_size=10) #thumb dip
+    pub_rh_thj2 = rospy.Publisher("/sh_rh_thj2_position_controller/command", Float64, queue_size=10)#thumb mip
+    pub_rh_thj3 = rospy.Publisher("/sh_rh_thj3_position_controller/command", Float64, queue_size=10)#thumb cmc flex
+    pub_rh_thj4 = rospy.Publisher("/sh_rh_thj4_position_controller/command", Float64, queue_size=10)#thumb cmc abd
+
+    pub_rh_wrj1 = rospy.Publisher("/sh_rh_wrj1_position_controller/command", Float64, queue_size=10) #wrist flex
+    pub_rh_wrj2 = rospy.Publisher("/sh_rh_wrj2_position_controller/command", Float64, queue_size=10)#wrist abd
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+# shadow robot's joint limits:
+# sh_rh_ffj0=  0 > 180
+# sh_rh_ffj3=  0 > 90
+# sh_rh_ffj4= -20 > 20
+# sh_rh_mfj0= 0 > 180
+# sh_rh_mfj3= 0 > 90
+# sh_rh_mfj4= -20 > 20
+# sh_rh_thj1=  0 > 90
+# sh_rh_thj2=  -40 > 40
+# sh_rh_thj3=  -12 > 12
+# sh_rh_thj4= 0 > 70
+# sh_rh_thj5= -60 > 60
+# sh_rh_wrj1=  -40 > 28
+# sh_rh_wrj2=  -30 > 10
--- src/serial_json_publisher_node.py
+++ src/serial_json_publisher_node.py
@@ -9,6 +9,7 @@ from geometry_msgs.msg import Point
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseArray
 from std_msgs.msg import Header
+import json
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=10)		#ttyUSB0
 
 temp_pose=Pose()
@@ -47,7 +48,7 @@ def talker():
 	pub_S13 = rospy.Publisher('/imu_S13', Point, queue_size=1)
 	pubArray = rospy.Publisher('/imu_pub_array', PoseArray, queue_size=1)
 	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(100) # 10hz
+	rate = rospy.Rate(1000) # 10hz
 	# ser = serial.Serial('/dev/ttyUSB0')  # open serial port
 	# print(ser.name)  # check which port was really used
 	ser.close()
@@ -67,6 +68,13 @@ def talker():
 		# x = ser.read()  # read one byte
 		# s = ser.read(10)  # read up to ten bytes (timeout)
 		line = ser.readline()  # read a '\n' terminated line
+		print("raw data:{}".format(line))
+		if len(line)>13:
+			dataj = json.loads(line)
+			print(dataj)
+			print(dataj['S0'])
+
+		# print type(dataj)
 		# rospy.loginfo(line)
 		# print("rawdata:".format(line))
 
@@ -76,80 +84,80 @@ def talker():
 		# print("ch0:".format(hex(ord(line[1]))))
 
 		# TODO check length first
-		if len(line)>13:
-
-			S0AZ = lowbyte_highbyte_to_float(line[1], line[2])
-			S0AY = lowbyte_highbyte_to_float(line[3], line[4])
-			S0AX = lowbyte_highbyte_to_float(line[5], line[6])
-			S0BZ = lowbyte_highbyte_to_float(line[7], line[8])
-			S0BY = lowbyte_highbyte_to_float(line[9], line[10])
-			S0BX = lowbyte_highbyte_to_float(line[11], line[12])
-			# print("S0AX: {}, Y: {} ,Z: {}; S0BX: {}, Y:{}, Z:{}".format(S0AX,S0AY,S0AZ,S0BX,S0BY,S0BZ))
-			Sensor_poseA = Pose()
-			Sensor_poseA.position.x = float(S0AX) / 10
-			Sensor_poseA.position.y = float(S0AY) / 10
-			Sensor_poseA.position.z = float(S0AZ) / 10
-
-			Sensor_poseB = Pose()
-			Sensor_poseB.position.x = float(S0BX) / 10
-			Sensor_poseB.position.y = float(S0BY) / 10
-			Sensor_poseB.position.z = float(S0BZ) / 10
-
-			# sensorA_point=Point()
-			# sensorA_point.x =float(S0AX)/10
-			# sensorA_point.y =float(S0AY)/10
-			# sensorA_point.z =float(S0AZ)/10
-			# pub.publish(sensorA_point)
-
-			if ord(line[0])==176:
-				# print("rawdata:".format(line))
-				rospy.loginfo(line)
-			elif ord(line[0])==160:
-				lst[0] = Sensor_poseA
-				lst[1] = Sensor_poseB
-				pub_S0.publish(Sensor_poseA.position)
-				pub_S1.publish(Sensor_poseB.position)
-			elif ord(line[0])==161:
-				lst[2] = Sensor_poseA
-				lst[3] = Sensor_poseB
-				pub_S2.publish(Sensor_poseA.position)
-				pub_S3.publish(Sensor_poseB.position)
-			elif ord(line[0]) == 162:
-				lst[4] = Sensor_poseA
-				lst[5] = Sensor_poseB
-				pub_S4.publish(Sensor_poseA.position)
-				pub_S5.publish(Sensor_poseB.position)
-			elif ord(line[0]) == 163:
-				lst[6] = Sensor_poseA
-				lst[7] = Sensor_poseB
-				pub_S6.publish(Sensor_poseA.position)
-				pub_S7.publish(Sensor_poseB.position)
-			elif ord(line[0]) == 164:
-				lst[8] = Sensor_poseA
-				lst[9] = Sensor_poseB
-				pub_S8.publish(Sensor_poseA.position)
-				pub_S9.publish(Sensor_poseB.position)
-			elif ord(line[0]) == 165:
-				lst[10] = Sensor_poseA
-				lst[11] = Sensor_poseB
-				pub_S10.publish(Sensor_poseA.position)
-				pub_S11.publish(Sensor_poseB.position)
-			elif ord(line[0]) == 166:
-				lst[12] = Sensor_poseA
-				lst[13] = Sensor_poseB
-				pub_S12.publish(Sensor_poseA.position)
-				pub_S13.publish(Sensor_poseB.position)
-
-			# else :
-			# 	line_index=ord(line[0])-160
-			# 	print("line index:{}".format(line_index))
-			# 	lst[line_index]=Sensor_pose
-
-			# myheader.stamp=
-			imu_array.header.stamp=rospy.get_rostime()
-			imu_array.header.frame_id='/world'
-			imu_array.poses=lst
-			pubArray.publish(imu_array)
+		# if len(line)>13:
+		#
+		# 	S0AZ = lowbyte_highbyte_to_float(line[1], line[2])
+		# 	S0AY = lowbyte_highbyte_to_float(line[3], line[4])
+		# 	S0AX = lowbyte_highbyte_to_float(line[5], line[6])
+		# 	S0BZ = lowbyte_highbyte_to_float(line[7], line[8])
+		# 	S0BY = lowbyte_highbyte_to_float(line[9], line[10])
+		# 	S0BX = lowbyte_highbyte_to_float(line[11], line[12])
+		# 	# print("S0AX: {}, Y: {} ,Z: {}; S0BX: {}, Y:{}, Z:{}".format(S0AX,S0AY,S0AZ,S0BX,S0BY,S0BZ))
+		# 	Sensor_poseA = Pose()
+		# 	Sensor_poseA.position.x = float(S0AX) / 10
+		# 	Sensor_poseA.position.y = float(S0AY) / 10
+		# 	Sensor_poseA.position.z = float(S0AZ) / 10
+		#
+		# 	Sensor_poseB = Pose()
+		# 	Sensor_poseB.position.x = float(S0BX) / 10
+		# 	Sensor_poseB.position.y = float(S0BY) / 10
+		# 	Sensor_poseB.position.z = float(S0BZ) / 10
+		#
+		# 	# sensorA_point=Point()
+		# 	# sensorA_point.x =float(S0AX)/10
+		# 	# sensorA_point.y =float(S0AY)/10
+		# 	# sensorA_point.z =float(S0AZ)/10
+		# 	# pub.publish(sensorA_point)
+		#
+		# 	if ord(line[0])==176:
+		# 		# print("rawdata:".format(line))
+		# 		rospy.loginfo(line)
+		# 	elif ord(line[0])==160:
+		# 		lst[0] = Sensor_poseA
+		# 		lst[1] = Sensor_poseB
+		# 		pub_S0.publish(Sensor_poseA.position)
+		# 		pub_S1.publish(Sensor_poseB.position)
+		# 	elif ord(line[0])==161:
+		# 		lst[2] = Sensor_poseA
+		# 		lst[3] = Sensor_poseB
+		# 		pub_S2.publish(Sensor_poseA.position)
+		# 		pub_S3.publish(Sensor_poseB.position)
+		# 	elif ord(line[0]) == 162:
+		# 		lst[4] = Sensor_poseA
+		# 		lst[5] = Sensor_poseB
+		# 		pub_S4.publish(Sensor_poseA.position)
+		# 		pub_S5.publish(Sensor_poseB.position)
+		# 	elif ord(line[0]) == 163:
+		# 		lst[6] = Sensor_poseA
+		# 		lst[7] = Sensor_poseB
+		# 		pub_S6.publish(Sensor_poseA.position)
+		# 		pub_S7.publish(Sensor_poseB.position)
+		# 	elif ord(line[0]) == 164:
+		# 		lst[8] = Sensor_poseA
+		# 		lst[9] = Sensor_poseB
+		# 		pub_S8.publish(Sensor_poseA.position)
+		# 		pub_S9.publish(Sensor_poseB.position)
+		# 	elif ord(line[0]) == 165:
+		# 		lst[10] = Sensor_poseA
+		# 		lst[11] = Sensor_poseB
+		# 		pub_S10.publish(Sensor_poseA.position)
+		# 		pub_S11.publish(Sensor_poseB.position)
+		# 	elif ord(line[0]) == 166:
+		# 		lst[12] = Sensor_poseA
+		# 		lst[13] = Sensor_poseB
+		# 		pub_S12.publish(Sensor_poseA.position)
+		# 		pub_S13.publish(Sensor_poseB.position)
+		#
+		# 	# else :
+		# 	# 	line_index=ord(line[0])-160
+		# 	# 	print("line index:{}".format(line_index))
+		# 	# 	lst[line_index]=Sensor_pose
+		#
+		# 	# myheader.stamp=
+		# 	imu_array.header.stamp=rospy.get_rostime()
+		# 	imu_array.header.frame_id='/world'
+		# 	imu_array.poses=lst
+		# 	pubArray.publish(imu_array)
 		rate.sleep()
 
 if __name__ == '__main__':
